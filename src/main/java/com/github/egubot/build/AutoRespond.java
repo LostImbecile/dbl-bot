LostImbecile/DBL-Egubot

Added a small feature for autorespond.

src/main/java/com/github/egubot/build/AutoRespond.java
@@ -1,5 +1,7 @@
 package com.github.egubot.build;
 
+import java.util.Random;
+
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.Messageable;
@@ -12,6 +14,8 @@ public class AutoRespond extends OnlineDataManager {
 	private static String idKey = "Responses_Message_ID";
 	private static String resourcePath = "/MessageResponses.txt";
 	private static String parameterSplit = ">>>>>>>>>>";
+
+	private Random rng = new Random();
 
 	public AutoRespond(DiscordApi api) throws Exception {
 		super(api, idKey, resourcePath, "Autorespond", true);
@@ -26,27 +30,35 @@ public class AutoRespond extends OnlineDataManager {
 		Message reference;
 		String[] responseContents;
 		boolean replyFlag = false;
+		boolean deleteFlag = false;
 		for (int i = 0; i < getData().size(); i++) {
 			responseContents = getData().get(i).replace("\t", "").split(parameterSplit);
+			responseContents[0] = responseContents[0].strip().toLowerCase();
+			responseContents[2] = responseContents[2].strip();
 
-			if (responseContents[1].strip().toLowerCase().matches("normal.*")) {
-				if (responseContents[0].strip().toLowerCase().matches("equal.*")) {
-					if (msgText.equalsIgnoreCase(responseContents[2].strip()))
+			if (responseContents[1].strip().equalsIgnoreCase("normal")) {
+				if (responseContents[0].equals("equal")) {
+					if (msgText.equalsIgnoreCase(responseContents[2]))
 						replyFlag = true;
 
-				} else if (responseContents[0].strip().toLowerCase().matches("contain.*")) {
-					if (msgText
-							.matches("(.*)(?i)(?<!\\w)" + responseContents[2].strip().toLowerCase() + "(?![\\w-])(.*)"))
+				} else if (responseContents[0].equals("contain")) {
+					if (msgText.matches(".*(?i)(?<![\\w-.])(?:" + responseContents[2] + ")(?![\\w-.]).*"))
 						replyFlag = true;
 
-				} else if (responseContents[0].strip().toLowerCase().matches("match.*")) {
-					if (msgText.matches(responseContents[2].strip().toLowerCase()))
+				} else if (responseContents[0].equals("match")) {
+					if (msgText.matches("(?i)" + responseContents[2]))
 						replyFlag = true;
 
+				} else if (responseContents[0].equals("msg delete")) {
+					if (msgText.matches(".*" + responseContents[2] + ".*"))
+						deleteFlag = true;
+				} else if (responseContents[0].equals("user delete")) {
+					if (e.getMessageAuthor().getIdAsString().matches(responseContents[2].replaceAll("[<>@]", "")))
+						deleteFlag = true;
 				}
 
 				if (replyFlag) {
-					responseContents[3] = responseContents[3].replace("%n", "\n").replaceAll(":<(.*)>:", ":$1:");
+					responseContents[3] = parseResponse(responseContents[3]);
 					if (e.getMessage().getMessageReference().isPresent()) {
 						reference = e.getMessage().getMessageReference().get().getMessage().get();
 						reference.reply(responseContents[3], false);
@@ -63,6 +75,12 @@ public class AutoRespond extends OnlineDataManager {
 					}
 					return true;
 				}
+
+				if (deleteFlag) {
+					e.getMessage().delete();
+					return true;
+				}
+
 			} else if (responseContents[1].strip().equalsIgnoreCase("Embed")) {
 
 			} else if (responseContents[1].strip().equalsIgnoreCase("Special")) {
@@ -75,11 +93,19 @@ public class AutoRespond extends OnlineDataManager {
 		return false;
 	}
 
-	public void writeResponse(String msgText, Messageable e) {
+	private String parseResponse(String response) {
+		String[] randomOptions = response.split("\\?\\?");
+		if (randomOptions.length > 1) {
+			response = randomOptions[rng.nextInt(randomOptions.length)];
+		}
+		return response.replace("%n", "\n").replaceAll(":<(.*)>:", ":$1:");
+	}
+
+	public void writeResponse(String msgText, Messageable e, boolean isOwner) {
 		try {
 			String newResponse = msgText.substring("b-response create".length()).strip();
 
-			newResponse = reformatResponse(newResponse);
+			newResponse = reformatResponse(newResponse, isOwner);
 			boolean isNameExist = false;
 
 			if (!isResponseValid(newResponse))
@@ -100,8 +126,9 @@ public class AutoRespond extends OnlineDataManager {
 			}
 		} catch (Exception e1) {
 			e.sendMessage("Correct format:"
-					+ "\nb-response create type >> message >> response (>> reaction1 >> reaction2 >>...)" + "\n\nTypes:"
-					+ "\nContain, equal and match");
+					+ "\nb-response create type >> message >> response (>> reaction1 >> reaction2 >>...)"
+					+ "\n\nAlternative:\nb-response create type >> message >> option1 ?? option2 ?? ... >> ..."
+					+ "\n\nTypes:" + "\nContain, equal and match");
 		}
 
 	}
@@ -114,20 +141,23 @@ public class AutoRespond extends OnlineDataManager {
 		return true;
 	}
 
-	private String reformatResponse(String st) {
+	private String reformatResponse(String st, boolean isOwner) {
 		st = st.replaceFirst(">>", ">> Normal >>");
 		st = st.replace(">>", parameterSplit);
+		st = st.replace("\n", "%n");
 		String lowcase = st.toLowerCase();
-		if (lowcase.matches("equal.*") || lowcase.matches("contain.*") || lowcase.matches("match.*"))
+		if (lowcase.equals("equal") || lowcase.equals("contain") || lowcase.equals("match"))
 			return st;
-		else
-			return null;
+		if(isOwner && (lowcase.equals("msg delete") || lowcase.equals("user delete")))
+			return st + " " + parameterSplit + " Delete";
+		
+		return null;
 	}
 
 	public void removeResponse(String msgText, Messageable e, boolean isOwner) {
 		try {
 			int startIndex = getLockedDataEndIndex();
-			
+
 			if (isOwner)
 				startIndex = 0;
 
@@ -164,12 +194,18 @@ public class AutoRespond extends OnlineDataManager {
 
 		return token[2].strip();
 	}
+	
+	public static String getResponseType(String st) {
+		String[] token = st.replace("\n", "").split(parameterSplit);
+
+		return token[0].strip().toLowerCase();
+	}
 
 	public static boolean isResponseEqual(String st, String st2) {
 		st = getResponseInvocation(st).toLowerCase().replace(" ", "");
 		st2 = getResponseInvocation(st2).toLowerCase().replace(" ", "");
-
-		return st.matches(st2 + "(.*)") || st2.matches(st + "(.*)");
+		
+		return st.matches(st2) || st2.matches(st) || st.equals(st2);
 	}
 
 }

LostImbecile/dbl-bot

src/main/java/com/github/egubot/build/AutoRespond.java
@@ -1,5 +1,7 @@
 package com.github.egubot.build;
 
+import java.util.Random;
+
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.Messageable;
@@ -12,6 +14,8 @@ public class AutoRespond extends OnlineDataManager {
 	private static String idKey = "Responses_Message_ID";
 	private static String resourcePath = "/MessageResponses.txt";
 	private static String parameterSplit = ">>>>>>>>>>";
+
+	private Random rng = new Random();
 
 	public AutoRespond(DiscordApi api) throws Exception {
 		super(api, idKey, resourcePath, "Autorespond", true);
@@ -26,27 +30,35 @@ public class AutoRespond extends OnlineDataManager {
 		Message reference;
 		String[] responseContents;
 		boolean replyFlag = false;
+		boolean deleteFlag = false;
 		for (int i = 0; i < getData().size(); i++) {
 			responseContents = getData().get(i).replace("\t", "").split(parameterSplit);
+			responseContents[0] = responseContents[0].strip().toLowerCase();
+			responseContents[2] = responseContents[2].strip();
 
-			if (responseContents[1].strip().toLowerCase().matches("normal.*")) {
-				if (responseContents[0].strip().toLowerCase().matches("equal.*")) {
-					if (msgText.equalsIgnoreCase(responseContents[2].strip()))
+			if (responseContents[1].strip().equalsIgnoreCase("normal")) {
+				if (responseContents[0].equals("equal")) {
+					if (msgText.equalsIgnoreCase(responseContents[2]))
 						replyFlag = true;
 
-				} else if (responseContents[0].strip().toLowerCase().matches("contain.*")) {
-					if (msgText
-							.matches("(.*)(?i)(?<!\\w)" + responseContents[2].strip().toLowerCase() + "(?![\\w-])(.*)"))
+				} else if (responseContents[0].equals("contain")) {
+					if (msgText.matches(".*(?i)(?<![\\w-.])(?:" + responseContents[2] + ")(?![\\w-.]).*"))
 						replyFlag = true;
 
-				} else if (responseContents[0].strip().toLowerCase().matches("match.*")) {
-					if (msgText.matches(responseContents[2].strip().toLowerCase()))
+				} else if (responseContents[0].equals("match")) {
+					if (msgText.matches("(?i)" + responseContents[2]))
 						replyFlag = true;
 
+				} else if (responseContents[0].equals("msg delete")) {
+					if (msgText.matches(".*" + responseContents[2] + ".*"))
+						deleteFlag = true;
+				} else if (responseContents[0].equals("user delete")) {
+					if (e.getMessageAuthor().getIdAsString().matches(responseContents[2].replaceAll("[<>@]", "")))
+						deleteFlag = true;
 				}
 
 				if (replyFlag) {
-					responseContents[3] = responseContents[3].replace("%n", "\n").replaceAll(":<(.*)>:", ":$1:");
+					responseContents[3] = parseResponse(responseContents[3]);
 					if (e.getMessage().getMessageReference().isPresent()) {
 						reference = e.getMessage().getMessageReference().get().getMessage().get();
 						reference.reply(responseContents[3], false);
@@ -63,6 +75,12 @@ public class AutoRespond extends OnlineDataManager {
 					}
 					return true;
 				}
+
+				if (deleteFlag) {
+					e.getMessage().delete();
+					return true;
+				}
+
 			} else if (responseContents[1].strip().equalsIgnoreCase("Embed")) {
 
 			} else if (responseContents[1].strip().equalsIgnoreCase("Special")) {
@@ -75,11 +93,19 @@ public class AutoRespond extends OnlineDataManager {
 		return false;
 	}
 
-	public void writeResponse(String msgText, Messageable e) {
+	private String parseResponse(String response) {
+		String[] randomOptions = response.split("\\?\\?");
+		if (randomOptions.length > 1) {
+			response = randomOptions[rng.nextInt(randomOptions.length)];
+		}
+		return response.replace("%n", "\n").replaceAll(":<(.*)>:", ":$1:");
+	}
+
+	public void writeResponse(String msgText, Messageable e, boolean isOwner) {
 		try {
 			String newResponse = msgText.substring("b-response create".length()).strip();
 
-			newResponse = reformatResponse(newResponse);
+			newResponse = reformatResponse(newResponse, isOwner);
 			boolean isNameExist = false;
 
 			if (!isResponseValid(newResponse))
@@ -100,8 +126,9 @@ public class AutoRespond extends OnlineDataManager {
 			}
 		} catch (Exception e1) {
 			e.sendMessage("Correct format:"
-					+ "\nb-response create type >> message >> response (>> reaction1 >> reaction2 >>...)" + "\n\nTypes:"
-					+ "\nContain, equal and match");
+					+ "\nb-response create type >> message >> response (>> reaction1 >> reaction2 >>...)"
+					+ "\n\nAlternative:\nb-response create type >> message >> option1 ?? option2 ?? ... >> ..."
+					+ "\n\nTypes:" + "\nContain, equal and match");
 		}
 
 	}
@@ -114,20 +141,23 @@ public class AutoRespond extends OnlineDataManager {
 		return true;
 	}
 
-	private String reformatResponse(String st) {
+	private String reformatResponse(String st, boolean isOwner) {
 		st = st.replaceFirst(">>", ">> Normal >>");
 		st = st.replace(">>", parameterSplit);
-		String lowcase = st.toLowerCase();
-		if (lowcase.matches("equal.*") || lowcase.matches("contain.*") || lowcase.matches("match.*"))
+		st = st.replace("\n", "%n");
+		String responseType = getResponseType(st);
+		if (responseType.equals("equal") || responseType.equals("contain") || responseType.equals("match"))
 			return st;
-		else
-			return null;
+		if(isOwner && (responseType.equals("msg delete") || responseType.equals("user delete")))
+			return st + " " + parameterSplit + " Delete";
+		
+		return null;
 	}
 
 	public void removeResponse(String msgText, Messageable e, boolean isOwner) {
 		try {
 			int startIndex = getLockedDataEndIndex();
-			
+
 			if (isOwner)
 				startIndex = 0;
 
@@ -164,12 +194,18 @@ public class AutoRespond extends OnlineDataManager {
 
 		return token[2].strip();
 	}
+	
+	public static String getResponseType(String st) {
+		String[] token = st.replace("\n", "").split(parameterSplit);
+
+		return token[0].strip().toLowerCase();
+	}
 
 	public static boolean isResponseEqual(String st, String st2) {
 		st = getResponseInvocation(st).toLowerCase().replace(" ", "");
 		st2 = getResponseInvocation(st2).toLowerCase().replace(" ", "");
-
-		return st.matches(st2 + "(.*)") || st2.matches(st + "(.*)");
+		
+		return st.matches(st2) || st2.matches(st) || st.equals(st2);
 	}
 
 }