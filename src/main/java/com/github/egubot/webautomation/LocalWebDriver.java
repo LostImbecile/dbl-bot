5f52815d9a8d06b42ec2629b65d1175b493db7f9
LostImbecile <171248575+Lostlmbecile@users.noreply.github.com>
2024-09-04 14:28:21 +0300

    Modified chromedriver arguments

--- src/main/java/com/github/egubot/webautomation/LocalWebDriver.java
+++ src/main/java/com/github/egubot/webautomation/LocalWebDriver.java
@@ -18,7 +18,8 @@ public class LocalWebDriver implements AutoCloseable {
 	JavascriptExecutor js;
 	Map<String, Object> vars;
 
-	// Set these up in your config.properties file (make sure to double up on slashes)
+	// Set these up in your config.properties file (make sure to double up on
+	// slashes)
 	// If you don't it's not an issue, this also doesn't affect non-headless mode
 	private static String userDataDirectory = ConfigManager.getProperty("User_Data_Directory");
 	private static String userProfile = ConfigManager.getProperty("User_Profile_Name");
@@ -55,6 +56,19 @@ public class LocalWebDriver implements AutoCloseable {
 		options.addArguments("--disable-notifications");
 		options.addArguments("--disable-infobars");
 		options.addArguments("--disable-popup-blocking");
+		
+		options.addArguments("--dns-prefetch-disable");
+		options.addArguments("--enable-cdp-events");
+		options.addArguments("--disable-web-security");
+		options.addArguments("--disable-features=NetworkService");
+		options.addArguments("--disable-features=SidePanelPinning");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("-–disable-hang-monitor");
+		
+		options.addArguments("--disable-blink-features=AutomationControlled");
+		options.addArguments("-–disable-features=TranslateUI,BlinkGenPropertyTrees");
+		options.addArguments("--disable-ipc-flooding-protection");
+		
 		System.setProperty("webdriver.chrome.silentOutput", "true"); // Suppress WebDriver logs
 
 		options.addArguments("--no-sandbox");
@@ -70,14 +84,14 @@ public class LocalWebDriver implements AutoCloseable {
 			options.addArguments("--disable-software-rasterizer");
 		}
 
+		options.addArguments("--disable-search-engine-choice-screen");
+
 		if (isHeadless) {
 			// Some pages will be blank in headless mode, this is to circumvent it
-			options.addArguments("""
-				--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-				 AppleWebKit/537.36 (KHTML, like Gecko)\
-				 Chrome/121.0.0.0 Safari/537.36""");
-			
-			options.addArguments("--headless");
+			options.addArguments(
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");
+
+			options.addArguments("--headless=new");
 			if (userDataDirectory != null && userProfile != null) {
 				options.addArguments("--user-data-dir=" + userDataDirectory);
 				options.addArguments("--profile-directory=" + userProfile);
